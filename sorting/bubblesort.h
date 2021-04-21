cfef118123a87575053ceefc732b4aaae892c61b
Aditya Prakash <prakashadityaap@gmail.com>
2021-04-21 08:43:55 +0530

    Added bubble sort

---
+++ sorting/bubblesort.h
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x, int *y) { 
+    int temp = *x; 
+    *x = *y; 
+    *y = temp; 
+} 
+
+void bubbleSort(int arr[], int n) { 
+    int i, j; 
+    for (i = 0; i < n-1; i++){
+        for (j = 0; j < n-i-1; j++){
+            if (arr[j] > arr[j+1]){
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+} 
+
+void printArray(int arr[], int size) { 
+    int i; 
+    for (i = 0; i < size; i++){
+        cout << arr[i] << " "; 
+    }
+    cout << "\n"; 
+} 
